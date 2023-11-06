15929943bd01c19983885dc5ffc876b7f63158b5
shen <2567176308@qq.com>
2023-11-06 13:56:00 +0800

    add backend project

---
+++ meteManager-backend/src/main/java/com/shen/meteManagerbackend/MeteManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shen.meteManagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeteManagerBackendApplication.class, args);
+	}
+
+}
---
+++ meteManager-backend/src/test/java/com/shen/meteManagerbackend/MeteManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shen.meteManagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
